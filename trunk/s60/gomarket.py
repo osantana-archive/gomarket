329770d4f28494ea81a1953c54ed36294fc7026c
osantana <osantana@b5e755e8-fd74-11dd-802b-177316e4ac06>
2009-02-24 07:24:17 +0000

    - finish 'stores' tab refactoring - implement the urllib funcions - tests and more tests...

--- trunk/s60/gomarket.py
+++ trunk/s60/gomarket.py
@@ -3,167 +3,502 @@
 
 import os
 import sys
+import md5
 sys.path.append("E:\\Python")
+
+import e32dbm
 
 from skel import *
 
 APP_NAME = u"gomarket"
 APP_VERSION = u"0.1"
+RESOURCE_DIR = "E:\\Python"
+
+COUNTRIES_TEST = {
+    u'key_a': u'Brazil',
+    u'key_b': u'USA',
+}
+STATES_TEST = {
+    u'key_a': {
+        u'key_aa': u'Sao Paulo',
+        u'key_ab': u'Rio de Janeiro',
+    },
+    u'key_b': {
+        u'key_ba': u'Florida',
+        u'key_bb': u'Alabama',
+    },
+}
+CITIES_TEST = {
+    u'key_aa': {
+        u'key_aaa': u'Sao Paulo',
+        u'key_aab': u'Sao Jose do Rio Preto',
+    },
+    u'key_ab': {
+        u'key_aba': u'Rio de Janeiro',
+        u'key_abb': u'Niteroi',
+    },
+    u'key_ba': {
+        u'key_baa': u'Miami',
+        u'key_bab': u'Orlando',
+    },
+    u'key_bb': {
+        u'key_bba': u'Montgomery',
+        u'key_bbb': u'Jackson',
+    },
+}
+
+STORES_TEST = {
+    u'key_aaa': {
+        u'key_aaaa': u'Supermercado Extra Jaguare\nRua Foo',
+        u'key_aaab': u'Supermercado Extra Pinheiros\nRua Bar',
+    },
+}
+
+PRICES_TEST = {
+    u'key_aaab': {
+        u'978': '10.90',
+    },
+}
+
 
 class DataManager(object):
-    _instance = None
+    def __init__(self, resourcedir):
+        self.resourcedir = resourcedir
 
-    def __init__(self):
-        self._locations = {
-            u'Brazil': {
-                u'São Paulo': [
-                    u'São Paulo',
-                    u'São José do Rio Preto',
-                ],
-                u'Rio de Janeiro': [
-                    u'Rio de Janeiro',
-                    u'Niterói',
-                ],
-            },
-            u'USA': {
-                u'Massachusetts': [
-                    u'Boston',
-                    u'Springfield',
-                ],
-            },
-        }
+        self.config = e32dbm.open(os.path.join(self.resourcedir, 'config.db'), 'nf')
+        self._cache_countries = e32dbm.open(os.path.join(self.resourcedir, 'countries.db'), 'nf')
+        # self._cache_ean_prefix = e32dbm.open(os.path.join(self.resourcedir, 'eanprefix.db'), 'nf')
+        self._cache_states = e32dbm.open(os.path.join(self.resourcedir, 'states.db'), 'nf')
+        self._cache_cities = e32dbm.open(os.path.join(self.resourcedir, 'cities.db'), 'nf')
+        self._cache_stores = e32dbm.open(os.path.join(self.resourcedir, 'stores.db'), 'nf')
 
-        self.config = {
-            'country': u'Brazil',
-            'state': u'São Paulo',
-            'city': u'São Paulo',
-        }
+    # country
+    def set_country(self, country):
+        self.config[u'country'] = country
+        self.state = u''
+        self.city = u''
+        self.store = u''
+        self._cache_states.clear()
+        self._cache_cities.clear()
+        self._cache_stores.clear()
+        self._cache_states.sync()
+        self._cache_cities.sync()
+        self._cache_stores.sync()
+    def get_country(self):
+        if not self.config.has_key(u'country'):
+            self.config[u'country'] = u''
+        return self.config[u'country']
+    country = property(get_country, set_country)
 
+    def get_country_name(self):
+        if not self._cache_countries.has_key(self.country):
+            return u''
+        return unicode(self._cache_countries[self.country])
+    country_name = property(get_country_name)
+
+    def _request_countries(self):
+        # TODO: urllib...
+        print 'Getting country list...'
+        return COUNTRIES_TEST
+    def add_country(self, name):
+        # TODO: urllib post and get new key...
+        key = md5.md5(name).hexdigest()
+        COUNTRIES_TEST[unicode(key)] = unicode(name)
+        self._cache_countries[unicode(key)] = unicode(name)
+        return key
 
     def countries(self):
-        country_list = self._locations.keys()
-        country_list.sort()
+        if not len(self._cache_countries):
+            self._cache_countries.update(self._request_countries())
+            self._cache_countries.sync()
+        country_list = [ (unicode(k), unicode(v)) for k,v in self._cache_countries.items() ]
+        country_list.sort(lambda x,y: cmp(x[1],y[1]))
         return country_list
 
+    # state
+    def set_state(self, state):
+        self.config[u'state'] = state
+        self.city = u''
+        self.store = u''
+        self._cache_cities.clear()
+        self._cache_stores.clear()
+        self._cache_cities.sync()
+        self._cache_stores.sync()
+    def get_state(self):
+        if not self.config.has_key(u'state'):
+            self.config[u'state'] = u''
+        return self.config[u'state']
+    state = property(get_state, set_state)
+
+    def get_state_name(self):
+        if not self._cache_states.has_key(self.state):
+            return u''
+        return unicode(self._cache_states[self.state])
+    state_name = property(get_state_name)
+
+    def _request_states(self, country):
+        # TODO: urllib...
+        print 'Getting state list of %s...' % (country,)
+        return STATES_TEST[country]
+    def add_state(self, name):
+        # TODO: urllib post and get new key...
+        key = md5.md5(name).hexdigest()
+        STATES_TEST[self.country][unicode(key)] = unicode(name)
+        self._cache_states[unicode(key)] = unicode(name)
+        return key
+
     def states(self):
-        state_list = self._locations[self.config['country']].keys()
-        state_list.sort()
+        country = self.country
+        if not len(self._cache_states):
+            self._cache_states.update(self._request_states(country))
+            self._cache_states.sync()
+        state_list = [ (unicode(k), unicode(v)) for k,v in self._cache_states.items() ]
+        state_list.sort(lambda x,y: cmp(x[1],y[1]))
         return state_list
 
+    # city
+    def set_city(self, city):
+        self.config[u'city'] = city
+        self.store = u''
+        self._cache_stores.clear()
+        self._cache_stores.sync()
+    def get_city(self):
+        if not self.config.has_key(u'city'):
+            self.config[u'city'] = u''
+        return self.config[u'city']
+    city = property(get_city, set_city)
+
+    def get_city_name(self):
+        if not self._cache_cities.has_key(self.city):
+            return u''
+        return unicode(self._cache_cities[self.city])
+    city_name = property(get_city_name)
+
+    def _request_cities(self, state):
+        # TODO: urllib...
+        print 'Getting city list...'
+        return CITIES_TEST[state]
+    def add_city(self, name):
+        # TODO: urllib post and get new key...
+        key = md5.md5(name).hexdigest()
+        CITIES_TEST[unicode(key)] = unicode(name)
+        self._cache_cities[unicode(key)] = unicode(name)
+        return key
+
     def cities(self):
-        city_list = self._locations[self.config['country']][self.config['state']]
-        city_list.sort()
+        state = self.state
+        if not len(self._cache_cities):
+            self._cache_cities.update(self._request_cities(state))
+            self._cache_cities.sync()
+        city_list = [ (unicode(k), unicode(v)) for k,v in self._cache_cities.items() ]
+        city_list.sort(lambda x,y: cmp(x[1],y[1]))
         return city_list
 
-    def get_instance(cls):
-        if cls._instance is None:
-            cls._instance = cls()
-        return cls._instance
-    get_instance = classmethod(get_instance)
+    # store
+    def set_store(self, store):
+        self.config[u'store'] = store
+    def get_store(self):
+        if not self.config.has_key(u'store'):
+            self.config[u'store'] = u''
+        return self.config[u'store']
+    store = property(get_store, set_store)
 
-    def save(self):
-        pass
+    def get_store_name(self):
+        if not self._cache_stores.has_key(self.store):
+            return u''
+        return unicode(self._cache_stores[self.store].split('\n')[0])
+        
+    def get_store_address(self):
+        if not self._cache_stores.has_key(self.store):
+            return u''
+        try:
+            return unicode(self._cache_stores[self.store].split('\n')[1])
+        except IndexError:
+            return u''
+            
+    def _request_stores(self, city):
+        # TODO: urllib...
+        print 'Getting store list...'
+        return STORES_TEST[city]
+    def add_store(self, name, address):
+        # TODO: urllib post and get new key...
+        key = md5.md5(name).hexdigest()
+        value = unicode(name) + u'\n' + unicode(address)
+        STORES_TEST[unicode(key)] = value
+        self._cache_stores[unicode(key)] = value
+        return key
+        
+    def stores(self):
+        city = self.city
+        if not len(self._cache_stores):
+            self._cache_stores.update(self._request_stores(city))
+            self._cache_stores.sync()
+
+        ret = []
+        for k, v in self._cache_stores.items():
+            name, address = v.split("\n")
+            ret.append( (unicode(k), unicode(name), unicode(address)) )
+        ret.sort(lambda x,y: cmp(x[1],y[1]))
+
+        return ret
+
+    def _get_store_name(self):
+        return self._cache_stores[self.config[u'store']]
+    store_name = property(_get_store_name)
+
+
+    # def get_ean13_prefix(self):
+    #     return self._ean_prefix[self.config['country']]
+
+    def close(self):
+        self.config.close()
+        self._cache_countries.close()
+        # self._cache_ean_prefix.close()
+        self._cache_states.close()
+        self._cache_cities.close()
+        self._cache_stores.close()
 
     def refresh(self):
+        # TODO
         pass
+
+    def get_prices(self, barcode, price, description):
+        # TODO
+        if barcode == 978:
+            return [
+                (u'Supermercado Foo', u'R$10,00'),
+                (u'Supermercado Bar', u'R$10,00'),
+                (u'Supermercado Baz', u'R$10,00'),
+            ]
+        else:
+            return []
+
 
 
 
 class MainApp(AppSkel):
     """This is the Application class"""
 
-    def setup(self):
+    TAB_SETTINGS = 0
+    TAB_STORES = 1
+    TAB_RESULTS = 2
+
+    SETTING_COUNTRY = 0
+    SETTING_STATE = 1
+    SETTING_CITY = 2
+
+    def init(self):
         self.title = u"GoMarket"
+        self.datamanager = DataManager(RESOURCE_DIR)
 
-    def tab_settings(self):
-        datamanager = DataManager.get_instance()
+        self.settings_lb = None
+        self.stores_lb = None
+        self.results_lb = None
 
+    def setup(self):
+        start_tab = self.TAB_SETTINGS
+        if self.datamanager.city:
+            start_tab = self.TAB_STORES
+        if self.datamanager.store:
+            start_tab = self.TAB_RESULTS
+        self.activate_tab(start_tab)
+
+    def tabs(self):
+        return (
+            (u'Settings', self.tab_settings_body),
+            (u'Stores', self.tab_stores_body),
+            (u'Result', self.tab_results_body, self.tab_results_menu),
+        )
+
+    def menu(self):
+        return [
+            (u'Refresh', self.refresh),
+        ]
+
+
+    # TAB_SETTINGS
+    def tab_settings_body(self):
         self.settings_list = [
-            (u'Country', datamanager.config['country']),
-            (u'State', datamanager.config['state']),
-            (u'City', datamanager.config['city']),
+            (u'Country', self.datamanager.country_name),
+            (u'State', self.datamanager.state),
+            (u'City', self.datamanager.city),
         ]
         self.settings_lb = appuifw.Listbox(self.settings_list, self.tab_callback_settings)
         return self.settings_lb
 
     def tab_callback_settings(self):
         index = self.settings_lb.current()
-        datamanager = DataManager.get_instance()
-        row = self.settings_list[index]
+        dataman = self.datamanager
 
+        if index == self.SETTING_COUNTRY:
+            self._setting('country',
+                dataman.countries,
+                dataman.get_country_name,
+                dataman.set_country,
+                dataman.add_country,
+                [ self.SETTING_STATE, self.SETTING_CITY ]
+            )
+        elif index == self.SETTING_STATE:
+            self._setting('state',
+                dataman.states,
+                dataman.get_state_name,
+                dataman.set_state,
+                dataman.add_state,
+                [ self.SETTING_CITY ]
+            )
+        else:
+            self._setting('city',
+                dataman.cities,
+                dataman.get_city_name,
+                dataman.set_city,
+                dataman.add_city,
+                [ ]
+            )
+            if dataman.city:
+                self.activate_tab(self.TAB_STORES)
+            
+    def _setting(self, option, datasrc, get_name, set_attr, add_item, reset_list):
+        index = self.settings_lb.current()
+        option_keys = [None]
+        option_values = [u'NEW']
         try:
-            if row[0] == "Country":
-                options = datamanager.countries()
-            elif row[0] == "State":
-                options = datamanager.states()
-            elif row[0] == "City":
-                options = datamanager.cities()
-            else:
-                raise ValueError("Invalid option '%s'." % (selected[0],))
+            for key, value in datasrc():
+                option_keys.append(key)
+                option_values.append(value)
         except KeyError:
-            options = []
-        options.insert(0, u'NEW ')
-
-        selection = appuifw.selection_list(options, 1)
+            pass # only NEW option
+            
+        selection = appuifw.selection_list(option_values, 1)
         if selection is None:
             return
-        option = options[selection]
+        key = option_keys[selection]
+        value = option_values[selection]
 
-        if option == u'NEW ':
-            option = appuifw.query(u"New %s" % row[0], 'text')
+        if key is None:
+            value = appuifw.query(u"New %s" % (option,), 'text')
+            if value is None:
+                return
+            key = add_item(value)
 
-        self.settings_list[index] = (row[0], option)
-        for i in range(index + 1, len(self.settings_list)):
-            self.settings_list[i] = (self.settings_list[i][0], u'')
+        set_attr(key)
+        
+        # reset next options
+        self.settings_list[index] = (self.settings_list[index][0], get_name())
+        for reset in reset_list:
+            self.settings_list[reset] = (self.settings_list[reset][0], u'')
         self.settings_lb.set_list(self.settings_list, index)
+    # /TAB_SETTINGS
 
-        datamanager.config['country'] = self.settings_list[0][1]
-        datamanager.config['state'] = self.settings_list[1][1]
-        datamanager.config['city'] = self.settings_list[2][1]
-        # TODO datamanager.save()
 
-    def tab_result(self):
-        results = appuifw.Listbox([u'result'], self.tab_callback_result)
-        return results
-
-    def tab_callback_result(self):
-        pass
-
-    def tab_stores(self):
-        results = appuifw.Listbox([u'stores'], self.tab_callback_stores)
-        return results
+    # TAB_STORES
+    def tab_stores_body(self):
+        self.load_stores_list()
+        self.stores_lb = appuifw.Listbox(self.store_items, self.tab_callback_stores)
+        return self.stores_lb
 
     def tab_callback_stores(self):
-        pass
+        index = self.stores_lb.current()
+
+        if self.store_keys[index] is None:
+            try:
+                name, address = appuifw.multi_query(u'Store Name:', u'Address:')
+            except TypeError: # cancel
+                return
+            if not name.strip():
+                return
+
+            key = self.datamanager.add_store(name, address)
+
+            self.load_stores_list()
+            self.refresh_stores_lb()
+        else:
+            self.datamanager.config['store'] = self.store_keys[index]
+
+        # move to result if it already exists
+        if self.results_lb:
+            result_list = self.get_results_list([])
+            self.refresh_results_lb(result_list)
+        self.activate_tab(self.TAB_RESULTS)
+
+    def load_stores_list(self):
+        self.store_keys = [None]
+        self.store_items = [ (u"NEW", u"") ]
+        for key, name, address in self.datamanager.stores():
+            self.store_keys.append(key)
+            self.store_items.append( (name, address) )
+
+    def refresh_stores_lb(self):
+        self.stores_lb.set_list(self.store_items)
+    # /TAB_STORES
 
 
-    def tabs(self):
-        tabs = (
-            (u'Settings', self.tab_settings),
-            (u'Stores', self.tab_stores),
-            (u'Result', self.tab_result),
-        )
-        return tabs
+    # TAB_RESULTS
+    def tab_results_body(self):
+        results_list = self.get_results_list([])
+        self.results_lb = appuifw.Listbox(results_list, self.tab_callback_results)
+        return self.results_lb
 
-    def menu(self):
+    def tab_results_menu(self):
         return [
-            (u'Get Price', self.get_price),
-            (u'Refresh', self.refresh),
+            (u'Get Prices', self.get_prices),
         ]
 
-    def get_price(self):
-        pass
+    def tab_callback_results(self):
+        if self.results_lb.current() == 0:
+            self.get_prices()
+
+    def get_results_list(self, results, barcode=""):
+        if barcode:
+            results_list = [ (u'Get Price', u"%s / %s" % (self.datamanager.store_name, barcode)) ]
+        else:
+            results_list = [ (u'Get Price', self.datamanager.store_name) ]
+        results_list += results
+        return results_list
+
+    def refresh_results_lb(self, results):
+        self.results_lb.set_list(results)
+    # /TAB_RESULTS
+
+
+    # handle callbacks...
+    def get_prices(self):
+        if not self.datamanager.config['store']:
+            appuifw.note(u"You need to select a store.", "error")
+            self.activate_tab(self.TAB_STORES)
+            return
+
+        barcode = appuifw.query(u"Barcode:", 'number') #, self.datamanager.get_ean13_prefix())
+        if barcode is None:
+            return
+        price = appuifw.query(u"Price:", 'float')
+        if not price:
+            return
+
+        description = appuifw.query(u"Description (optional):", 'text')
+        if description is None:
+            description = u""
+
+        prices = self.datamanager.get_prices(barcode, price, description)
+        if not prices:
+            appuifw.note(u'No prices for code %s' % (barcode,), 'info')
+        results = self.get_results_list(prices, barcode)
+        self.refresh_results_lb(results)
 
     def refresh(self):
-        datamanager = DataManager.get_instance()
-        datamanager.refresh()
-        datamanager.save()
+        self.datamanager.refresh()
 
-    def menu_help(self):
-        pass
-
+    def confirm_exit(self):
+        self.datamanager.close()
+        return True
     def menu_about(self):
-        pass
+        self.about_dialog(
+            name=u"GoMarket",
+            version=u"0.1",
+            year=u'2009',
+            authors=[ u"Osvaldo Santana Neto", u"Ramiro Luz" ],
+            icon="E:\\Python\\gomarket_icon.png",
+            license=u"MIT License",
+        )
 
 def main():
     app = MainApp()
--- trunk/s60/skel.py
+++ trunk/s60/skel.py
@@ -2,37 +2,41 @@
 
 import e32
 import appuifw
+import topwindow
+import graphics
 
 class AppSkel(object):
-    TAB_TITLE = 0
-    TAB_CREATE = 1
-    TAB_BODY = 2
+    _TAB_TITLE = 0
+    _TAB_CREATE_BODY = 1
+    _TAB_CREATE_MENU = 2
+    _TAB_BODY = 3
+    _TAB_MENU = 4
+
+    selected_tab = 0
+
     def __init__(self):
+        # initialization
+        self.init()
+
         self._mainlock = e32.Ao_lock()
-        
+
         # tabs
         self._tabs = []
         if self.tabs():
             appuifw.app.set_tabs(
-                self._create_tabs(self.tabs()),
+                self._create_tabs(),
                 self._internal_tab_callback)
-            self._internal_tab_callback(0)
         else:
             appuifw.app.body = self.body()
+            appuifw.app.menu = self._create_menu()
 
         # menu
-        menu = list(self.menu())
-        if hasattr(self, 'menu_help'):
-            menu.append( (u'Help', self.menu_help) )
-        if hasattr(self, 'menu_about'):
-            menu.append( (u'About', self.menu_about) )
-        menu.append( (u'Exit', self.quit) )
-        appuifw.app.menu = menu
         appuifw.app.exit_key_handler = self.quit
-        
+
+
         # final setup
         self.setup()
-        
+
     # Methods to be reimplemented
     def body(self):
         "This method returns the body object of UI."
@@ -45,30 +49,56 @@ class AppSkel(object):
     def confirm_exit(self):
         "This method will be called before exit. Return False to abort exit."
         return True
-        
+
     def tabs(self):
         """Return a sequence of (title, callback) tuple for tabs or
         an empty sequence for no tabs (default)"""
         return []
-        
+
     def menu(self):
         """Return a sequence of (title, callback|submenu) tupple for
         menu options."""
         return []
     #/
 
-    def _create_tabs(self, tabs):
+    def _create_menu(self):
+        menu = list(self.menu())
+        if hasattr(self, 'menu_help'):
+            menu.append( (u'Help', self.menu_help) )
+        if hasattr(self, 'menu_about'):
+            menu.append( (u'About', self.menu_about) )
+        menu.append( (u'Exit', self.quit) )
+        return menu
+
+    def _create_tabs(self):
+        tabs = self.tabs()
+        
         titles = []
-        for title, create_callback in tabs:
-            self._tabs.append( [ title, create_callback, None ] )
+        for tab in tabs:
+            title = tab[0]
+            if len(tab) == 3:
+                self._tabs.append( [ title, tab[1], tab[2], None, None ] )
+            else:
+                self._tabs.append( [ title, tab[1], None, None, None ] )
             titles.append(title)
         return titles
-        
+
     def _internal_tab_callback(self, index):
         tab = self._tabs[index]
-        if tab[AppSkel.TAB_BODY] is None:
-            tab[AppSkel.TAB_BODY] = tab[AppSkel.TAB_CREATE]()
-        appuifw.app.body = tab[AppSkel.TAB_BODY]
+        if tab[AppSkel._TAB_BODY] is None:
+            tab[AppSkel._TAB_BODY] = tab[AppSkel._TAB_CREATE_BODY]()
+        if tab[AppSkel._TAB_MENU] is None:
+            if tab[AppSkel._TAB_CREATE_MENU]:
+                tab[AppSkel._TAB_MENU] = tab[AppSkel._TAB_CREATE_MENU]() + self._create_menu()
+            else:
+                tab[AppSkel._TAB_MENU] = self._create_menu()
+        appuifw.app.body = tab[AppSkel._TAB_BODY]
+        appuifw.app.menu = tab[AppSkel._TAB_MENU]
+
+    def activate_tab(self, index):
+        self._internal_tab_callback(index)
+        appuifw.app.activate_tab(index)
+        self.selected_tab = index
 
     def _get_title(self):
         return appuifw.app.title
@@ -76,10 +106,67 @@ class AppSkel(object):
         appuifw.app.title = title
     title = property(_get_title, _set_title)
 
+    def _center_text(self, d_canvas, y, msg, font='dense'):
+        box, pixels, chars = d_canvas.measure_text(msg, font=font)
+        txt_size = (box[2] - box[0], box[3] - box[1])
+        y += txt_size[1] + 6
+        d_canvas.text(((d_canvas.size[0]-txt_size[0])/2,y), msg, font=font)
+        return y
+
+    def about_dialog(self, name, version, year, authors, icon, license):
+        self._about_lock = e32.Ao_lock()
+        self._old_exit = appuifw.app.exit_key_handler
+        self._old_menu = appuifw.app.menu
+
+        size, pos = appuifw.app.layout(appuifw.EScreen)
+
+        self._old_orientation = appuifw.app.orientation
+        if appuifw.app.orientation != 'portrait': # TODO: implement dialog box for landscape UI
+            appuifw.app.orientation = 'portrait'
+
+        appuifw.app.menu = [(u'Close', self._exit_about_dialog), ]
+        appuifw.app.exit_key_handler = self._exit_about_dialog
+
+        d_size = (size[0] - 23, size[1] - 100)
+        d_pos = (10, (size[1] - d_size[1]) / 2)
+        d_canvas = graphics.Image.new(d_size)
+        d_canvas.clear(0xFFFFFF)
+        y = 6
+
+        icon = graphics.Image.open(icon)
+        d_canvas.blit(icon, target=((d_size[0]-icon.size[0])/2,y))
+        y += icon.size[1]
+        
+        y = self._center_text(d_canvas, y, name, 'title')
+        y = self._center_text(d_canvas, y, version, 'legend')
+        y = self._center_text(d_canvas, y + 6, u"Copyright © " + year, 'dense')
+        for author in authors:
+            y = self._center_text(d_canvas, y, author, 'dense')
+        y = self._center_text(d_canvas, y + 6, license, 'legend')
+
+        self._about_window = topwindow.TopWindow()
+        self._about_window.add_image(d_canvas, (0,0))
+
+        self._about_window.position = d_pos
+        self._about_window.size = d_size
+        self._about_window.shadow = 3
+        self._about_window.corner_type = 'square'
+        self._about_window.show()
+
+        self._about_lock.wait()
+
+    def _exit_about_dialog(self):
+        appuifw.app.menu = self._old_menu
+        appuifw.app.exit_key_handler = self._old_exit
+        appuifw.app.orientation = self._old_orientation
+        self._about_window.hide()
+        self._about_lock.signal()
+
     def run(self):
         self._mainlock.wait()
-        
+
     def quit(self):
         if not self.confirm_exit():
             return
+        appuifw.app.set_tabs([], None)
         self._mainlock.signal()
